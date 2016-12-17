42cd29e347f46e861d29df87b53109421e72082f
ajstark123 <albert@ajstark.org>
2016-12-17 09:58:48 -0500

    Add Logger and Server (Shell)  standard Input, Standard Output, Standard error inferstructure

--- LinuxShell_InputOutput/src/main/java/org/ajstark/LinuxShell/InputOutput/InputOutputData.java
+++ LinuxShell_InputOutput/src/main/java/org/ajstark/LinuxShell/InputOutput/InputOutputData.java
@@ -1,18 +1,22 @@
 package org.ajstark.LinuxShell.InputOutput;
 
+import java.io.*;
+
 /**
  * Created by Albert on 11/4/16.
  *
  * @version $Id$
  *
  */
-public class InputOutputData {
+public class InputOutputData implements Serializable {
     private String  data;
     private boolean lastDataSent;
+    private String  uuidStr;
 
     public InputOutputData( String  data ) {
         this.data            = data;
         this.lastDataSent    = false;
+        this.uuidStr         = "CONSOLE";
     }
 
     public InputOutputData( ) {
@@ -26,5 +30,13 @@ public class InputOutputData {
 
     public String getData() {
         return data;
+    }
+    
+    public void setUuidStr( String uuidStr ) {
+        this.uuidStr = uuidStr;
+    }
+    
+    public String getUuidStr() {
+        return uuidStr;
     }
 }
