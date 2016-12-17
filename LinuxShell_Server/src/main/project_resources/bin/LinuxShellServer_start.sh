09b1dc989c8b9e7889aea5003d9c4bf720741948
ajstark123 <albert@ajstark.org>
2016-12-17 10:12:15 -0500

    Add Logger and Server (Shell)  standard Input, Standard Output, Standard error inferstructure

--- LinuxShell_Server/src/main/project_resources/bin/LinuxShellServer_start.sh
+++ LinuxShell_Server/src/main/project_resources/bin/LinuxShellServer_start.sh
@@ -18,10 +18,6 @@ export LOGS_DIR=../logs
 
 export LOGGER_GONFIG_FILE=$LIB_HOME/properties/Logger.properties
 
-# -DLoggerConfigurationFile=/Users/Albert/Documents/Development/build/LinuxShellBuild/LinuxShell_Dist-1.0-SNAPSHOT/lib/properties/Logger.properties
-# -Dlog4j.configuration=/Users/Albert/Documents/Development/build/LinuxShellBuild/LinuxShell_Dist-1.0-SNAPSHOT/lib/properties/Logger.properties
-
-# export LOGGER_CONFIG="-DLoggerConfigurationFile=$LOGGER_GONFIG_FILE -Dlog4j.configurationFile=$LOGGER_GONFIG_FILE"
 export LOGGER_CONFIG="-DLoggerConfigurationFile=$LOGGER_GONFIG_FILE"
 
 
