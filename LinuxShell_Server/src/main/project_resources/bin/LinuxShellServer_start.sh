648b68f95b1217fbc5370d11c39b6978c6ba6ebd
ajstark123 <albert@ajstark.org>
2016-12-17 10:04:05 -0500

    Add Logger and Server (Shell)  standard Input, Standard Output, Standard error inferstructure

--- LinuxShell_Server/src/main/project_resources/bin/LinuxShellServer_start.sh
+++ LinuxShell_Server/src/main/project_resources/bin/LinuxShellServer_start.sh
@@ -10,9 +10,21 @@ export LIB_HOME=../lib
 export SERVER_LIB=$LIB_HOME/LinuxShell/LinuxShell_Server.jar
 export IN_OUT_LIB=$LIB_HOME/LinuxShell/LinuxShell_InputOutput.jar
 
-export LOGGER_LIB=$LIB_HOME/Logger
+export LOGGER_LIB=$LIB_HOME/log4j
+export LOG4J_API=$LOGGER_LIB/log4j-api-2.7.jar
+export LOG4J_CORE=$LOGGER_LIB/log4j-core-2.7.jar
 
 export LOGS_DIR=../logs
 
-java -classpath $SERVER_LIB:$IN_OUT_LIB:$LOGGER_LIB  org.ajstark.LinuxShell.Shell.LinuxShellServer
+export LOGGER_GONFIG_FILE=$LIB_HOME/properties/Logger.properties
+
+# -DLoggerConfigurationFile=/Users/Albert/Documents/Development/build/LinuxShellBuild/LinuxShell_Dist-1.0-SNAPSHOT/lib/properties/Logger.properties
+# -Dlog4j.configuration=/Users/Albert/Documents/Development/build/LinuxShellBuild/LinuxShell_Dist-1.0-SNAPSHOT/lib/properties/Logger.properties
+
+# export LOGGER_CONFIG="-DLoggerConfigurationFile=$LOGGER_GONFIG_FILE -Dlog4j.configurationFile=$LOGGER_GONFIG_FILE"
+export LOGGER_CONFIG="-DLoggerConfigurationFile=$LOGGER_GONFIG_FILE"
+
+
+java $LOGGER_CONFIG -DInputOutputType=CONSOLE -classpath $SERVER_LIB:$IN_OUT_LIB:$LOG4J_API:$LOG4J_CORE  org.ajstark.LinuxShell.Shell.LinuxShellServer
+
 
