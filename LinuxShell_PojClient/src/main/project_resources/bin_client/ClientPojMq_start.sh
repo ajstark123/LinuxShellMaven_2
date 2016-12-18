c7999edf40e07fbf95f59d651cd5c10723b6e193
ajstark123 <albert@ajstark.org>
2016-12-18 12:37:40 -0500

    publish via rabbitmq to the shell and shell reading from rabbit mw

---
+++ LinuxShell_PojClient/src/main/project_resources/bin_client/ClientPojMq_start.sh
@@ -0,0 +1,48 @@
+
+
+echo " "
+echo " "
+echo "Starting the Client POJ MQ"
+echo " "
+echo " "
+
+
+
+
+
+export LIB_HOME=../lib
+
+#
+#  Linux Shell Jars and Client Jars
+#
+export CLIENT_LIB=$LIB_HOME/LinuxShellClient/LinuxShell_PojClient.jar
+export IN_OUT_LIB=$LIB_HOME/LinuxShell/LinuxShell_InputOutput.jar
+
+#
+#  Logger jar files
+#
+export LOGGER_LIB=$LIB_HOME/log4j
+export LOG4J_API=$LOGGER_LIB/log4j-api-2.7.jar
+export LOG4J_CORE=$LOGGER_LIB/log4j-core-2.7.jar
+
+#
+# Logger Configuration
+#
+export LOGGER_GONFIG_FILE=$LIB_HOME/properties/LoggerClient.properties
+export LOGGER_CONFIG="-DLoggerConfigurationFile=$LOGGER_GONFIG_FILE"
+
+#
+#  RABBIT MQ JAR
+#
+export RABBITMQ_LIB=$LIB_HOME/rabbitmq
+export RABBIT_MQ=$RABBITMQ_LIB/amqp-client-4.0.0.jar
+
+
+#
+# slf4j
+#
+export SLF4_LIB=$LIB_HOME/slf4j
+export SLF4=$SLF4_LIB/slf4j-api-1.7.22.jar:$SLF4_LIB/slf4j-simple-1.7.22.jar
+
+
+java $LOGGER_CONFIG -DInputOutputType=MQ -DQueueName=org.ajstark.LinuxShell.input -classpath $CLIENT_LIB:$IN_OUT_LIB:$SLF4:$RABBIT_MQ$SERVER_LIB:$IN_OUT_LIB:$RABBIT_MQ:$LOG4J_API:$LOG4J_CORE   org.ajstark.LinuxShell.ShellClient.PojMqClient
